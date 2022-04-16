58ef2fb50ef4a2d2e3ca681c643022be5d2f2fc4
IInfo <info@i1nfo.com>
2022-04-17 02:00:27 +0800

    command line args

--- main.py
+++
@@ -1,8 +0,0 @@
-from assignment import AssignmentManager
-from student import StudentInfoDict
-
-if __name__ == '__main__':
-    info_dict = StudentInfoDict("student-list.csv")
-    manager = AssignmentManager()
-    manager.process_package("Lab03-JUnit for Unit Test.zip", info_dict)
-    manager.check()
---
+++ normlab.py
@@ -0,0 +1,16 @@
+import sys
+
+from assignment import AssignmentManager
+from student import StudentInfoDict
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print(f"Usage:\npython normlab.py <student list> <Lab package>")
+        exit(0)
+    student_list_path = sys.argv[1]
+    package_path = sys.argv[2]
+
+    info_dict = StudentInfoDict(student_list_path)
+    manager = AssignmentManager()
+    manager.process_package(package_path, info_dict)
+    manager.check()
